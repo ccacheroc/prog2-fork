aea993d1c7fc553d9e2d6597aaec233df1698e05
Cristina Cachero <ccacheroc@gmail.com>
2025-02-18 11:35:16 +0100

    clase estudiante
    adicion de métodos

--- estudiante.py
+++ estudiante.py
@@ -6,3 +6,34 @@ class Estudiante:
         self.edad = edad
         self.cursos_inscritos = cursos_inscritos
         Estudiante.total_estudiantes += 1  # Aumenta el contador cada vez que se crea un estudiante
+
+    def inscribir_curso(self, curso):
+        self.cursos_inscritos.append(curso)
+        print(f"El estudiante {self.nombre} se ha inscrito en el curso {curso}")
+
+    def mostrar_informacion(self):
+        print(f"Nombre: {self.nombre}")
+        print(f"Edad: {self.edad}")
+        print("Cursos inscritos:")
+        for curso in self.cursos_inscritos:
+            print(f"- {curso}")
+        print()
+
+    def __str__(self):
+        return f"Nombre: {self.nombre}\nEdad: {self.edad}\nCursos inscritos: {self.cursos_inscritos}"
+
+
+    def __repr__(self):
+        return f'"Estudiante({self.nombre}, {self.edad}, {self.cursos_inscritos})"'
+
+    @classmethod
+    def desde_tupla(cls,tupla):
+        return cls(*tupla)
+
+    @staticmethod
+    def edad_valida(edad):
+        return str(edad).isdigit() and int(edad) > 18
+
+if __name__== "__main__":
+    e1 = Estudiante("Juan", 20, ["Matemáticas", "Física"])
+    print(e1)
