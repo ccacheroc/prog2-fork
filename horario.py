81e0fdea6bba227429fffd22b059f6a58d8f43fe
Cristina Cachero <ccacheroc@gmail.com>
2025-02-20 12:38:04 +0100

    cambio de clase curso por asignautra

--- horario.py
+++ horario.py
@@ -1,8 +1,7 @@
 class Horario:
     total_horarios = 0  # Contador de instancias
 
-    def __init__(self, curso, dia, hora_inicio, hora_fin):
-        self.curso = curso
+    def __init__(self,  dia, hora_inicio, hora_fin):
         self.dia = dia
         self.hora_inicio = hora_inicio
         self.hora_fin = hora_fin
